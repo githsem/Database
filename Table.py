fe33601a24e6c2263cd6130adb0943adae197e32
githsem <53379599+githsem@users.noreply.github.com>
2020-09-05 20:17:14 +0200

    update

--- Table.py
+++ Table.py
@@ -1,4 +1,10 @@
 import sqlite3
 con = sqlite3.connect("kutuphane.db")
 cursor = con.cursor()
+
+def tablo_olustur():
+    cursor.execute("CREATE TABLE IF NOT EXISTS kitaplik(Isim TEXT,Yazar TEXT,Yayinevi TEXT,Sayfa_Sayisi INT)")
+    con.commit()
+
+tablo_olustur()
 con.close()
