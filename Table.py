2bf17797f46a768ffb07a0724202aca6f4b5d94b
githsem <53379599+githsem@users.noreply.github.com>
2020-09-06 01:11:30 +0200

    update

--- Table.py
+++ Table.py
@@ -9,36 +9,42 @@ def tablo_olustur():
 def veri_ekle():
     cursor.execute("INSERT INTO kitaplik VALUES('Istanbul Hatirasi','Ahmet Umit','Everest',561)")
     con.commit()
+
 def veri_ekle2(isim,yazar,yayinevi,sayfa_sayisi):
     cursor.execute("INSERT INTO kitaplik VALUES(?,?,?,?)",(isim,yazar,yayinevi,sayfa_sayisi))
     con.commit()
+
 def veri_al():
     cursor.execute("SELECT * FROM kitaplik")
     liste = cursor.fetchall()
     print("Kitaplik Listeleri")
     for i in liste:
         print(i)
+
 def veri_al2():
     cursor.execute("SELECT Isim,Yazar FROM kitaplik")
     liste = cursor.fetchall()
     print("Kitaplik Listeleri")
     for i in liste:
         print(i)
+
 def veri_al3(yayinevi):
     cursor.execute("SELECT * FROM kitaplik Where Yayinevi = ?",(yayinevi,))
     liste = cursor.fetchall()
     print("Kitaplik Listeleri")
     for i in liste:
         print(i)
+
 def veri_guncelle(yeni,eski):
     cursor.execute("UPDATE kitaplik set Yayinevi = ? WHERE Yayinevi = ?",(yeni,eski))
     con.commit()
+
 def veri_sil(veri):
     cursor.execute("DELETE FROM kitaplik WHERE Yazar = ?",(veri,))
     con.commit()
 
 
-veri_sil("Ahmet Umit")
+veri_sil("Tess Gerritsen")
 veri_al()
 
 con.close()
