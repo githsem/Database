f17a4658996234814996f35a85c50088c1390b7a
githsem <53379599+githsem@users.noreply.github.com>
2020-09-05 20:42:42 +0200

    update

--- Table.py
+++ Table.py
@@ -6,5 +6,10 @@ def tablo_olustur():
     cursor.execute("CREATE TABLE IF NOT EXISTS kitaplik(Isim TEXT,Yazar TEXT,Yayinevi TEXT,Sayfa_Sayisi INT)")
     con.commit()
 
+def veri_ekle():
+    cursor.execute("INSERT INTO kitaplik VALUES('Istanbul Hatirasi','Ahmet Umit','Everest',561)")
+    con.commit()
+
 tablo_olustur()
+veri_ekle()
 con.close()
